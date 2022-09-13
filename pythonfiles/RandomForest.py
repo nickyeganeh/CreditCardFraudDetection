cecb58cf2419e27b7358cbde69391e50d5e25ce0
nickyeganeh <nicholas.yeganeh@comcast.net>
2022-09-13 09:35:16 -0700

    Python Code

---
+++ pythonfiles/RandomForest.py
@@ -0,0 +1,24 @@
+# THIS FILE IS TO TRAIN THE MODEL ONLY
+
+
+# IMPORTS
+import numpy as np
+import pandas as pd 
+from sklearn.metrics import f1_score
+from sklearm.metrics import precision_recall_fscore_support
+from sklean.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+# TRAIN AND TEST DATA
+X_train = np.array(pd.read_csv("../train-test_data/X_train.csv"))
+X_test = np.array(pd.read_csv("../train-test_data/X_test.csv"))
+y_train = np.array(pd.read_csv("../train-test_data/y_train.csv"))
+y_test = np.array(pd.read_csv("../train-test_data/y_test.csv"))
+
+# MODEL TRAINING
+rfc = RandomForestClassifier()
+rfc.fit(X_train, y_train)
+
+# SAVE MODEL
+joblib.dump(rfc, '../models/rfc.sav')
