1ff4006eacc424fc135e279884efe5506a7fcbf5
nickyeganeh <nicholas.yeganeh@comcast.net>
2022-09-13 16:13:31 -0700

    refinement

--- pythonfiles/ExtraTrees.py
+++ pythonfiles/ExtraTrees.py
@@ -5,30 +5,24 @@
 print("<<< IMPORTING MODULES >>>")
 import numpy as np
 import pandas as pd 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import ExtraTreesClassifier
 import joblib
 print("<<< MODULES ARE IMPORTED >>>")
 
+
 print("<<< LOADING TRAIN AND TEST DATA >>>")
 # TRAIN AND TEST DATA
-X = pd.read_csv("../train-test_data/X.csv")
-y = pd.read_csv("../train-test_data/y.csv").Class
+X_train = pd.read_csv("../train-test_data/X_train.csv")
+y_train = pd.read_csv("../train-test_data/y_train.csv").Class
 print("<<< TRAIN AND TEST DATA ARE LOADED >>>")
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .25, random_state = 1)
-
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.fit_transform(X_test)
-
 print("<<< TRAINING MODEL >>>")
 # MODEL TRAINING
-etc = ExtraTreesClassifier()
+etc = ExtraTreesClassifier(n_estimators = 100, random_state = 1)
 etc.fit(X_train, y_train)
 print("<<< MODEL IS TRAINED >>>")
+
 
 print("<<< SAVING MODEL >>>")
 # SAVE MODEL
