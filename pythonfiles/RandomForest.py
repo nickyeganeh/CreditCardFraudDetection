69aee4c252751a7815f319d5b3d7c76399f24b82
nickyeganeh <nicholas.yeganeh@comcast.net>
2022-09-13 09:51:05 -0700

    random forest model

--- pythonfiles/RandomForest.py
+++ pythonfiles/RandomForest.py
@@ -2,6 +2,7 @@
 
 
 # IMPORTS
+print("<<< IMPORTING MODULES >>>")
 import numpy as np
 import pandas as pd 
 from sklearn.metrics import f1_score
@@ -9,16 +10,24 @@ from sklearn.metrics import precision_recall_fscore_support
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 import joblib
+print("<<< MODULES ARE IMPORTED >>>")
 
+print("<<< LOADING TRAIN AND TEST DATA >>>")
 # TRAIN AND TEST DATA
 X_train = np.array(pd.read_csv("../train-test_data/X_train.csv"))
 X_test = np.array(pd.read_csv("../train-test_data/X_test.csv"))
 y_train = np.array(pd.read_csv("../train-test_data/y_train.csv"))
 y_test = np.array(pd.read_csv("../train-test_data/y_test.csv"))
+print("<<< TRAIN AND TEST DATA ARE LOADED >>>")
 
+print("<<< TRAINING MODEL >>>")
 # MODEL TRAINING
 rfc = RandomForestClassifier()
 rfc.fit(X_train, y_train)
+print("<<< MODEL IS TRAINED >>>")
 
+print("<<< SAVING MODEL >>>")
 # SAVE MODEL
 joblib.dump(rfc, '../models/rfc.sav')
+print("<<< MODEL SAVED >>>")
+print("<<< DONE >>>")
