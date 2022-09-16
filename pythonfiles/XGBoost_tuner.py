a539bd18b895de2b69e693ea4a2a6e95524a7164
nickyeganeh <nicholas.yeganeh@comcast.net>
2022-09-16 19:39:43 -0400

    fixed syntax error

--- pythonfiles/XGBoost_tuner.py
+++ pythonfiles/XGBoost_tuner.py
@@ -19,14 +19,14 @@ model = Pipeline([
     ])
 
 grid = GridSearchCV()
-params = {"n_estimators" = [100, 200, 300],
-		"learning_rate" = [0.05, 0.10, 0.15, 0.20, 0.25, 0.30],
-		"scale_pos_neg" = [1, 199007/357],
-		"max_depth" = [ 3, 4, 5, 6, 8, 10, 12, 15],
-		"min_child_weight" = [ 1, 3, 5, 7],
-		"gamma" = [ 0.0, 0.1, 0.2 , 0.3, 0.4],
-		"colsample_bytree" = [ 0.3, 0.4, 0.5 , 0.7]
-		"tree_method" = ["hist", "auto"]}
+params = {"n_estimators" : [100, 200, 300],
+		"learning_rate" : [0.05, 0.10, 0.15, 0.20, 0.25, 0.30],
+		"scale_pos_neg" : [1, 199007/357],
+		"max_depth" : [ 3, 4, 5, 6, 8, 10, 12, 15],
+		"min_child_weight" : [ 1, 3, 5, 7],
+		"gamma" : [ 0.0, 0.1, 0.2 , 0.3, 0.4],
+		"colsample_bytree" : [ 0.3, 0.4, 0.5 , 0.7]
+		"tree_method" : ["hist", "auto"]}
 
 grid = GridSearchCV(model, params, scoring = "f1_weighted", n_jobs = 4)
 grid.fit(pipline_X_train, pipeline_y_train)
