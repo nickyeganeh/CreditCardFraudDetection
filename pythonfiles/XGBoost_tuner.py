dd4b3e18cdcf396878478b3972afc981c2dae963
nickyeganeh <nicholas.yeganeh@comcast.net>
2022-09-19 12:28:01 -0400

    syntax error corrected

--- pythonfiles/XGBoost_tuner.py
+++ pythonfiles/XGBoost_tuner.py
@@ -22,7 +22,6 @@ model = Pipeline([
 
 params = {"classification__n_estimators"   : [100, 200, 300],
         "classification__scale_pos_neg"    : [1, 199007/357],
-        "classification__tree_method"      : ["hist", "auto"],
         "classification__learning_rate"    : [0.05, 0.10, 0.15, 0.20, 0.25, 0.30],
         "classification__max_depth"        : [ 3, 4, 5, 6, 8, 10, 12, 15],
         "classification__min_child_weight" : [ 1, 3, 5, 7 ],
@@ -30,7 +29,7 @@ params = {"classification__n_estimators"   : [100, 200, 300],
         "classification__colsample_bytree" : [ 0.3, 0.4, 0.5 , 0.7 ]}
 
 grid = RandomizedSearchCV(estimator = model, 
-                    param_grid = params, 
+                    param_distributions = params, 
                     scoring = "f1_weighted",
                     n_iter = 250,
                     cv = 5, 
